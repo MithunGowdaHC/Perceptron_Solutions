59fd54f7fac8da701b0ec65d4790de735a2a23e2
MithunGowda <mithungowdahc12a@gmail.com>
2024-11-18 14:27:35 +0530

    footer eight

--- src/user/pages/Login.jsx
+++ src/user/pages/Login.jsx
@@ -7,19 +7,7 @@
 //     <form
 //       onSubmit={(event) => {
 //         event.preventDefault();
-//       }}
-//       className=" flex flex-col items-center w-[90%] sm:max-w-96  m-auto  gap-4 text-gray-800"
-//     >
-//       <div className=" inline-flex items-center gap-2 mb-2 mt-[50%]">
-//         <p className=" prata-regular text-3xl">{currentState}</p>
-//         <hr className=" border-none h-[1.5px] w-8 bg-gray-800" />
-//       </div>
-//       {currentState === "Sign Up" ? (
-//         <input
-//           type="text"
-//           className=" w-full px-3 py-2 border border-gray-800 "
-//           placeholder="Name"
-//           required
+//    
 //         />
 //       ) : (
 //         ""
